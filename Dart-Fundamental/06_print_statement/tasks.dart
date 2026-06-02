163502890cf3d368bac4dc819a3585c87d280214
A'laa Mamdouh Ibrahim <mmdouhalaa80@gmail.com>
2026-06-02 22:50:12 +0300

    Update tasks.dart

--- Dart-Fundamental/06_print_statement/tasks.dart
+++ Dart-Fundamental/06_print_statement/tasks.dart
@@ -8,6 +8,7 @@ void main() {
   // Then print a sentence using all three with interpolation.
   // Example: "Hi! I'm Sara, 17 years old, from Cairo."
   // YOUR CODE HERE:
+  print("Hi! I'm Sara, 17 years old, from Cairo.");
 
 
   // Task 2:
@@ -17,6 +18,13 @@ void main() {
   // Sara          17      Alexandria
   // Mohamed       15      Giza
   // YOUR CODE HERE:
+  // first answer
+  print("Name \t Age \t City \n Ahmed \t 16 \t Cairo \n Sara \t 17 \t Alexandria \n Mohamed \t 15 \t Giza");
+  // second answer
+  // print("Name \t Age \t City");
+  // print("Ahmed \t 16 \t Cairo");
+  // print("Sara \t 17 \t Alexandria");
+  // print("Mohamed \t 15 \t Giza");
 
 
   // Task 3:
@@ -26,7 +34,17 @@ void main() {
   // ... up to 5 x 10 = 50
   // Use string interpolation and individual print statements.
   // YOUR CODE HERE:
-
+  int num = 5;
+  print('$num x 1 = ${num * 1}');
+  print('$num x 2 = ${num * 2}');
+  print('$num x 3 = ${num * 3}');
+  print('$num x 4 = ${num * 4}');
+  print('$num x 5 = ${num * 5}');
+  print('$num x 6 = ${num * 6}');
+  print('$num x 7 = ${num * 7}');
+  print('$num x 8 = ${num * 8}');
+  print('$num x 9 = ${num * 9}');
+  print('$num x 10 = ${num * 10}');
 
   // Task 4:
   // Print this banner using special characters:
@@ -34,5 +52,7 @@ void main() {
   // *   Welcome to Dart! *
   // **********************
   // YOUR CODE HERE:
+
+  print('********************** \n *   Welcome to Dart! * \n **********************');
 
 }
